487160d8ebcdb2c00d6537914c75d664553e73d1
raselmlbd <m.hasan@sekai-lab-bd.com>
2020-08-31 01:46:26 +0600

    added aws for deploy

---
+++ Quotes/build/precache-manifest.61561114ba6084035f5fd5f900dc692a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a030570ab39b00e278d4595494001b4a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "817a3dda38fc0e64f0e9",
+    "url": "/static/css/main.19dd6e22.chunk.css"
+  },
+  {
+    "revision": "aad9425b5692dc4e2f93",
+    "url": "/static/js/2.2f239cae.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2f239cae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "817a3dda38fc0e64f0e9",
+    "url": "/static/js/main.dc99094b.chunk.js"
+  },
+  {
+    "revision": "7f8c9a98401951139cac",
+    "url": "/static/js/runtime-main.036c173f.js"
+  }
+]);
