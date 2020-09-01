3b437b2e1cf7e2735e9dede810537404c22b0889
raselmlbd <m.hasan@sekai-lab-bd.com>
2020-09-01 20:28:06 +0600

    remove the cros

--- Quotes/src/API/api-call.js
+++ Quotes/src/API/api-call.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const APICalling = async () => {
 	return await axios(
-		"https://cors-anywhere.herokuapp.com/https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json"
+		"https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json"
 	);
 };
 
