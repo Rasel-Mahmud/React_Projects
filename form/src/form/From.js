7983063fe0fd75b6092241cf1bfeaf567ba537ac
raselmlbd <m.hasan@sekai-lab-bd.com>
2020-09-08 19:51:55 +0600

    simple form completed

---
+++ form/src/form/From.js
@@ -0,0 +1,152 @@
+import React, { useState } from "react";
+import "./../scss/Form.scss";
+
+const initialState = {
+	firstName: "",
+	lastName: "",
+	biography: "",
+	transport: "",
+	breakfast: false,
+	lunch: false,
+	dinner: false,
+	shirtSize: "",
+	agree: false,
+};
+
+const Form = () => {
+	const [formState, setFormState] = useState(initialState);
+
+	const onchangeForm = (e) => {
+		const value =
+			e.target.type === "checkbox" ? e.target.checked : e.target.value;
+		setFormState({
+			...formState,
+			[e.target.name]: value,
+		});
+	};
+
+	const formSubmitHandler = (e) => {
+		e.preventDefault();
+		console.log(formState);
+	};
+
+	return (
+		<form onSubmit={formSubmitHandler}>
+			<label htmlFor="firstName">First Name</label>
+			<input
+				type="text"
+				id="firstName"
+				name="firstName"
+				value={formState.firstName}
+				onChange={onchangeForm}
+			/>
+
+			<label htmlFor="lastName">Last Name</label>
+			<input
+				type="text"
+				id="lastName"
+				name="lastName"
+				value={formState.lastName}
+				onChange={onchangeForm}
+			/>
+
+			<label htmlFor="biography">Biography</label>
+			<textarea
+				name="biography"
+				id="biography"
+				rows="8"
+				onChange={onchangeForm}
+				value={formState.biography}
+			/>
+
+			<label htmlFor="transport">Preferred Transport</label>
+			<select
+				name="transport"
+				id="transport"
+				value={formState.transport}
+				onChange={onchangeForm}
+			>
+				<option value="">Select One</option>
+				<option value="bus">Bus</option>
+				<option value="train">Train</option>
+				<option value="planes">Planes</option>
+			</select>
+
+			<fieldset>
+				<legend>Select Your Meals</legend>
+				<label htmlFor="breakfast">Breakfast</label>
+				<input
+					type="checkbox"
+					id="breakfast"
+					name="breakfast"
+					checked={formState.breakfast}
+					onChange={onchangeForm}
+				/>
+
+				<label htmlFor="lunch">Lunch</label>
+				<input
+					type="checkbox"
+					id="lunch"
+					name="lunch"
+					checked={formState.lunch}
+					onChange={onchangeForm}
+				/>
+
+				<label htmlFor="dinner">Dinner</label>
+				<input
+					type="checkbox"
+					id="dinner"
+					name="dinner"
+					checked={formState.dinner}
+					onChange={onchangeForm}
+				/>
+			</fieldset>
+
+			<fieldset>
+				<legend>T-Shirt Size</legend>
+				<label htmlFor="sizeL">Large</label>
+				<input
+					type="radio"
+					id="sizeL"
+					name="shirtSize"
+					value="l"
+					onChange={onchangeForm}
+					checked={formState.shirtSize === "l"}
+				/>
+
+				<label htmlFor="sizeM">Medium</label>
+				<input
+					type="radio"
+					id="sizeM"
+					name="shirtSize"
+					value="m"
+					onChange={onchangeForm}
+					checked={formState.shirtSize === "m"}
+				/>
+
+				<label htmlFor="sizeS">Large</label>
+				<input
+					type="radio"
+					id="sizeS"
+					name="shirtSize"
+					value="s"
+					onChange={onchangeForm}
+					checked={formState.shirtSize === "s"}
+				/>
+			</fieldset>
+
+			<label htmlFor="agree">I Agree TOC</label>
+			<input
+				type="checkbox"
+				id="agree"
+				checked={formState.agree}
+				name="agree"
+				onChange={onchangeForm}
+			/>
+
+			<button type="submit">Submit</button>
+		</form>
+	);
+};
+
+export default Form;
