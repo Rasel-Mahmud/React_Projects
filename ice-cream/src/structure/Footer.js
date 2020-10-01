7187739785dd05c8918ecf8ed41882353f82b4e2
raselmlbd <m.hasan@sekai-lab-bd.com>
2020-10-01 21:25:45 +0600

    header and footer added

---
+++ ice-cream/src/structure/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Footer = () => (
+  <footer>
+    <span> &copy; 2020 | Ice Cream</span>
+  </footer>
+);
+
+export default Footer;
---
+++ ice-cream/src/structure/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import iceCreamImage from './../assets/img/ultimate-ice-cream.svg';
+
+const Header = () => (
+  <header>
+    <img src={iceCreamImage} alt="" />
+    <h1>Ice Cream</h1>
+  </header>
+);
+
+export default Header;
