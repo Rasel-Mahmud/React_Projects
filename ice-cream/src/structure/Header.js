680dda8338678b7f6ba326bd2b0924529d4c4ea8
raselmlbd <m.hasan@sekai-lab-bd.com>
2020-10-07 00:09:48 +0600

    menu added

--- ice-cream/src/structure/Header.js
+++ ice-cream/src/structure/Header.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import iceCreamImage from './../assets/img/ultimate-ice-cream.svg';
 
 const Header = () => (
   <header>
     <img src={iceCreamImage} alt="" />
     <h1>Ice Cream</h1>
+    <nav>
+      <NavLink to="/" activeClassName="active" exact>
+        Home
+      </NavLink>
+    </nav>
   </header>
 );
 
