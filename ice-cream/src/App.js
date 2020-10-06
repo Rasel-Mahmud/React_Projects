31c4fd6e9e49e4333d22ce94ec13586e2f23a560
raselmlbd <m.hasan@sekai-lab-bd.com>
2020-10-06 18:15:49 +0600

    route added for edit ice cream

--- ice-cream/src/App.js
+++ ice-cream/src/App.js
@@ -1,16 +1,19 @@
-import React, { Fragment } from 'react';
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Header from './structure/Header';
 import Footer from './structure/Footer';
 import IceCreamMenu from './ice-cream/iceCremMenu';
+import EditIceCream from './ice-cream/EditIceCream';
 import './styles/ice-cream.scss';
 
 const App = () => {
   return (
-    <Fragment>
+    <Router>
       <Header />
-      <IceCreamMenu />
+      <Route path="/" exact component={IceCreamMenu} />
+      <Route path="/menu-items/:menuItemId" component={EditIceCream} />
       <Footer />
-    </Fragment>
+    </Router>
   );
 };
 
---
+++ ice-cream/src/ice-cream/EditIceCream.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const EditIceCream = () => (
+  <main>
+    <h2>Edit ice-Cream</h2>
+  </main>
+);
+
+export default EditIceCream;
